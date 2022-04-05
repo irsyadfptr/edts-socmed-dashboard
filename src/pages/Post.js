6dce7445398100f3b7eeb39ac9975efe8ee52a76
irsyadputranto <irsyad.putranto@sg-edts.com>
2022-04-05 15:01:25 +0700

    Add post table

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import PostTable from '../components/PostTable'
 
 
 function Post() {
+	const users = useSelector(state => state.users.userList)
+  const posts = useSelector(state => state.posts.posts)
   return (
-    <div>Post</div>
-  )
+    <>
+	    <PostTable user={users} posts={posts} />
+    </>
+  );
+
 }
 
 export default Post
